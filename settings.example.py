4e88e844c8f1eb0e9c33d1b7ac63b40bbfdccc5c
zinglau <zinglau@users.noreply.github.com>
2019-09-26 12:21:31 +0800

    Fix typos and format

--- settings.example.py
+++ settings.example.py
@@ -13,9 +13,9 @@ TWILLO_AUTHY_API_KEY=""
 
 # Authy user id to send the push notifications to
 # This user must be added to your Twillo Authy application and set active
-AUTHY_USER_ID=
+AUTHY_USER_ID=0
 
-# Polling frequence in seconds
+# Polling frequency in seconds
 AUTHY_POLL_INTERVAL=5
 
 # Whether denied client should also be disabled to avoid further notifications
@@ -26,13 +26,15 @@ AUTHY_DISABLE_IF_DENIED=False
 
 ### BEGIN Telegram helper settings
 
-# This is given by @BotFather as HTTP API token when new bot is registerd (see https://core.telegram.org/bots#6-botfather)
+# This is given by @BotFather as HTTP API token when new bot is registered
+# See https://core.telegram.org/bots#6-botfather
 TG_TOKEN=""
 
 # TG user ID used to control the bot
-TG_USER_ID=
+TG_USER_ID=0
 
-# HTTP PROXY for the bot (see https://github.com/python-telegram-bot/python-telegram-bot/wiki/Working-Behind-a-Proxy)
+# Optional, HTTP PROXY for the bot if required
+# See https://github.com/python-telegram-bot/python-telegram-bot/wiki/Working-Behind-a-Proxy
 TG_REQUEST_KWARGS={
     #'proxy_url': 'http://ip:port/',
     # Optional, if you need authentication:
@@ -43,7 +45,7 @@ TG_REQUEST_KWARGS={
 # Number of async workers to run. One should be enough for personal use
 TG_WORKERS=1
 
-# Polling frequence in seconds
+# Polling frequency in seconds
 TG_POLL_INTERVAL=5.0
 
 ### END Telegram helper settings
